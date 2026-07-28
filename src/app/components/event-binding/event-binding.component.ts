331836100b1f955ea421b46ab636b2d9fb80cfd4
monju1125 <monju1125@github.com>
2026-07-28 03:33:00 +0000

    Test run: 10/15 passed, 5 failed, 0 skipped - 2026-07-28T03:33:00.427Z

--- src/app/components/event-binding/event-binding.component.ts
+++ src/app/components/event-binding/event-binding.component.ts
@@ -24,8 +24,9 @@ export class EventBindingComponent implements OnInit {
    * The method should be bound to the click event of button 2 via event binding.
    */
 
-
-
+processClickButton2(){
+  this.button2ClickedMessage ="button 2 clicked";
+}
 
   constructor() { }
 
